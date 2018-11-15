3dac6e5702922026f3efc05dc45acad7675a70b2
Josef Fagerstroem <josfa@amazon.com>
2018-11-15 16:16:36 +0100

    Re-add some out-commented code

--- summarize.py
+++ summarize.py
@@ -17,16 +17,13 @@ SCRAPERS_DIR = "scrapers"
 
 if __name__ == "__main__":
     # Automatically import and instantiate all scrapers
-    #scraper_files = [f for f in listdir(SCRAPERS_DIR) if isfile(join(SCRAPERS_DIR, f)) and f.endswith('_scraper.py')]
-    #scrapers = []
-    #for sf in scraper_files:
-    #    module_path = "%s.%s" % (SCRAPERS_DIR, sf[:-3])
-    #    importlib.import_module(module_path)
-    #    cls = [c for c in inspect.getmembers(sys.modules[module_path], inspect.isclass) if c[0] != 'Scraper'][0]
-    #    scrapers.append(cls[1]())
-    #for s in scrapers:
-    #    s.get_scrape_data()
-    #    s.close()
-    s = WillhemScraper()
-    s.get_scrape_data()
-    s.close()
+    scraper_files = [f for f in listdir(SCRAPERS_DIR) if isfile(join(SCRAPERS_DIR, f)) and f.endswith('_scraper.py')]
+    scrapers = []
+    for sf in scraper_files:
+        module_path = "%s.%s" % (SCRAPERS_DIR, sf[:-3])
+        importlib.import_module(module_path)
+        cls = [c for c in inspect.getmembers(sys.modules[module_path], inspect.isclass) if c[0] != 'Scraper'][0]
+        scrapers.append(cls[1]())
+    for s in scrapers:
+        scrape_data, errors = s.get_scrape_data()
+        s.close()
